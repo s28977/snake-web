9489134a6ca180df3a58794beb31cffe5a662e80
s28977 <s28977@pjwstk.edu.pl>
2024-06-20 13:03:35 +0200

    Created game.html file, which displays empty game board

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from packages.game import Snake
 
 app = Flask(__name__)
+
+game = Snake(10)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('game.html', grid_size=10)
 
 
 if __name__ == '__main__':
