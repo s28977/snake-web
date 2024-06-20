c723a67f6b2733b283e7221d17f8f1eacbc844f7
s28977 <s28977@pjwstk.edu.pl>
2024-06-20 13:55:22 +0200

    Added initial snake and food to game.html file

--- app.py
+++ app.py
@@ -8,9 +8,15 @@ game = Snake(10)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('game.html', grid_size=10)
+def start_game():  # put application's code here
+    global game
+    return render_template('game.html', grid_size=10, board=game.board)
 
+# @app.route('/make_move', methods=['POST'])
+# def make_move():
+#     global game
+#     data = request.json
+#     key = data['key']
 
 if __name__ == '__main__':
     app.run()
