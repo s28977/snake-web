3849be4cc74d9b37bea1e2ee0e6bba475b11d90c
s28977 <s28977@pjwstk.edu.pl>
2024-06-21 02:21:07 +0200

    Added upper case WSAD (only lowercase worked before) keys to control snake movement

--- packages/key_mapping.py
+++ packages/key_mapping.py
@@ -3,13 +3,13 @@ from packages.logic import Direction
 
 def map_to_direction(key):
     match key:
-        case "ArrowDown" | "s":
+        case "ArrowDown" | "s" | "S":
             direction = Direction.DOWN
-        case "ArrowUp" | "w":
+        case "ArrowUp" | "w" | "W":
             direction = Direction.UP
-        case "ArrowRight" | "d":
+        case "ArrowRight" | "d" | "D":
             direction = Direction.RIGHT
-        case "ArrowLeft" | "a":
+        case "ArrowLeft" | "a" | "A":
             direction = Direction.LEFT
         case _:
             direction = None
